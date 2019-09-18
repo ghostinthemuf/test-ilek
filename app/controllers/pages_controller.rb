06117b7a92b8df19d14d8cbf7de3fdea23c9a5e4
Antoine Gaillard <antoine@agaillard.fr>
2019-09-18 19:52:21 +0200

    parsing in private method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,19 +5,7 @@ class PagesController < ApplicationController
     File.open(Rails.root.join('public', 'uploads', 'uploaded_data'), 'wb') do |file|
       file.write(uploaded_file.read)
     end
-    if File.exist?('public/uploads/uploaded_data')
-      enedis_data = Nokogiri::XML(File.open('public/uploads/uploaded_data'))
-      enedis_data.root.xpath('Corps_de_fichier_par_PDL').each do |client|
-        client_data = {}
-        client_data['Identifiant PDL'] = client.xpath('Identifiant_Stable_PDL').text
-        client_data['Date début'] = client.xpath('Situation_Contrat/Date_Debut_Consommation').text
-        client_data['Date fin'] = client.xpath('Situation_Contrat/Date_Fin_Consommation').text
-        client_data['Index début'] = client.xpath('Situation_Contrat/Compteur/Periode/Donnees_Cadran/Index_Debut_Consommation').text
-        client_data['Index fin'] = client.xpath('Situation_Contrat/Compteur/Periode/Donnees_Cadran/Index_A_Facturer').text
-        client_data['Consommation'] = client.xpath('Situation_Contrat/Compteur/Periode/Donnees_Cadran/Consommation_Cadran').text.to_i
-        @clients_data << client_data
-      end
-    end
+    parsing if File.exist?('public/uploads/uploaded_data')
     respond_to do |format|
       format.js
     end
@@ -25,5 +13,21 @@ class PagesController < ApplicationController
 
   def parser
     @keys = ['Identifiant PDL', 'Date début', 'Date fin', 'Index début', 'Index fin', 'Consommation']
+  end
+
+  private
+
+  def parsing
+    enedis_data = Nokogiri::XML(File.open('public/uploads/uploaded_data'))
+    enedis_data.root.xpath('Corps_de_fichier_par_PDL').each do |client|
+      client_data = {}
+      client_data['Identifiant PDL'] = client.xpath('Identifiant_Stable_PDL').text
+      client_data['Date début'] = client.xpath('Situation_Contrat/Date_Debut_Consommation').text
+      client_data['Date fin'] = client.xpath('Situation_Contrat/Date_Fin_Consommation').text
+      client_data['Index début'] = client.xpath('Situation_Contrat/Compteur/Periode/Donnees_Cadran/Index_Debut_Consommation').text
+      client_data['Index fin'] = client.xpath('Situation_Contrat/Compteur/Periode/Donnees_Cadran/Index_A_Facturer').text
+      client_data['Consommation'] = client.xpath('Situation_Contrat/Compteur/Periode/Donnees_Cadran/Consommation_Cadran').text
+      @clients_data << client_data
+    end
   end
 end
